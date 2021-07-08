1a343ef696155edf1c0d25b5018377a671dc906e
btwq97 <benjamin.tan@outlook.sg>
2021-07-08 18:14:25 +0800

    added dark mode

--- javascript/main.js
+++ javascript/main.js
@@ -28,3 +28,72 @@ for (let i=0; i<fadein.length; i++) {
         initial[i].classList.toggle("hide");
     });
 }
+
+/* dark mode button */
+const darkMode = document.querySelector("#darkMode");
+const body = document.querySelector("body");
+const nav_link = document.querySelectorAll(".nav-link");
+const nav_logo = document.querySelector(".nav-logo");
+const underline = document.querySelectorAll(".parallelogram-underline");
+const side_block = document.querySelectorAll(".side-block");
+const pagination = document.querySelectorAll(".buttons");
+
+darkMode.addEventListener("click", changeMode);
+
+function changeMode() {
+  if (darkMode.checked) {
+    body.classList.remove("light");
+    nav_logo.classList.remove("light");
+    for (let i=0; i<underline.length; i++) {
+      underline[i].classList.remove("light");
+    }
+    for (let i=0; i<nav_link.length; i++) {
+      nav_link[i].classList.remove("light");
+    }
+    for (let i=0; i<side_block.length; i++) {
+      side_block[i].classList.remove("light");
+    }
+    for (let i=0; i<pagination.length; i++) {
+      pagination[i].classList.remove("light");
+    }
+  } 
+  else {
+    body.classList.add("light");
+    nav_logo.classList.add("light");
+    for (let i=0; i<underline.length; i++) {
+      underline[i].classList.add("light");
+    }
+    for (let i=0; i<nav_link.length; i++) {
+      nav_link[i].classList.add("light");
+    }
+    for (let i=0; i<side_block.length; i++) {
+      side_block[i].classList.add("light");
+    }
+    for (let i=0; i<pagination.length; i++) {
+      pagination[i].classList.add("light");
+    }
+  }
+}
+
+// const darkMode = document.querySelector("#darkMode");
+// darkMode.addEventListener("click", changeMode);
+
+// const parameters = document.querySelectorAll(
+//   "body",
+//   ".nav-link",
+//   ".nav-logo",
+//   ".parallelogram-underline",
+//   ".side-block"
+// );
+
+// function changeMode() {
+//   if (darkMode.checked) {
+//     for (let i=0; i<parameters.length; i++) {
+//       parameters[i].classList.remove("light");
+//     }
+//   } else {
+//     for (let i=0; i<parameters.length; i++) {
+//       parameters[i].classList.add("light");
+//     }
+//   }
+// }
